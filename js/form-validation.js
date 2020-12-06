d7218dbcba839c6050646967cfdc78bfb1fbac5b
Abdulaziz <abdulaziz_mirzaev_03@mail.ru>
2020-12-06 21:15:13 +0500

    Feedback page Validation Full + Modules Page

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,38 +1,53 @@
 // Access the form we have created
 const form  = document.forms["feedbackForm"];
-// Access the attributes we have created
-const name = document.getElementById('name');
-const nameError = document.querySelector('#name + p.message');
+var error = true;
 
-const surname = document.getElementById('surname');
-const surnameError = document.querySelector('#surname + p.message');
+for (let i = 0; i < form.length-1; i++) {
+    let field = form[i];
 
-const level = document.getElementById('level');
-const levelError = document.querySelector('#level + p.message');
+    field.addEventListener('input', function (event) {
+        validateField(field);
+    });
+}
 
-const comment = document.getElementById('comment');
-const commentError = document.querySelector('#comment + p.message');
-
-form.addEventListener('submit', function (event) {
-    // if the email field is valid, we let the form submit
-
+form.addEventListener('submit', async function (event) {
     // checking each form element for emptiness
+    let statusBox = document.querySelector(".feedback .block");
+    let statusBoxText = document.querySelector(".feedback .block h3");
+    statusBox.className = "block";
+    error = true;
+
     for (let i = 0; i < form.length-1; i++) {
-
         let field = form[i];
-        let value = field.value;
-        let errorTag = document.querySelector("#" + field.id + "+ p.message");
-        let fieldName = document.querySelector("label[for=" + field.id + "]");
-
-        if ( value.trim() == "" ) {
-            errorTag.textContent = fieldName.textContent + " cannot be blank";
-            errorTag.style.display = "block";
-            field.style.borderColor = "#d9534f"
-        }
+        validateField(field);
     }
 
-    event.preventDefault();
-
-
+    if ( error === false) {
+        statusBox.className += " danger";
+        statusBoxText.textContent = "Oops! You have some errors!";
+        event.preventDefault();
+    } else {
+        statusBox.className += " success";
+        statusBoxText.textContent = "Your feedback was successfully sent. Thank you!";
+        window.scrollTo(0,0);
+        alert("Your feedback was successfully sent. Thank you!");
+        window.open("index.html");
+    }
 });
+
+function validateField(field) {
+    let value = field.value;
+    let errorTag = document.querySelector("#" + field.id + "+ p.message");
+    let fieldName = document.querySelector("label[for=" + field.id + "]");
+
+    if ( value.trim() == "" ) { // Highlight error
+        errorTag.textContent = fieldName.textContent + " cannot be blank";
+        errorTag.style.display = "block";
+        field.style.borderColor = "#d9534f";
+        error = false;
+    } else {  // Success
+        field.style.borderColor = "#5cb85c";
+        errorTag.style.display = "none";
+    }
+}
 
